458a94ed9469af6d6b182d20e78603bc19204065
lucaGilardenghi <luca.gilardenghi@yahoo.co.uk>
2023-07-28 14:03:35 +0100

    added functionality to click buttons to alter what is displayed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import "./style.scss";
-
-// strings array to store text on buttons and assign grid area
+// Define global variables
 
 const buttonClassNames: string[] = [
   "cancel",
@@ -24,6 +23,18 @@ const buttonClassNames: string[] = [
   "decimal",
 ];
 
+const buttons: HTMLButtonElement[] = [];
+
+// test array
+
+let testNumbers = [2, 4, 6];
+
+/* DOM Element Variables */
+
+const screen = document.querySelector(".calculator__screen") as HTMLElement;
+/* ______________________________________________________________________________ */
+
+/* Function definitions */
 const giveButtonClass = (buttonClassNames: string[]) => {
   const buttons = document.querySelectorAll("button");
   let counter = 0;
@@ -33,4 +44,76 @@ const giveButtonClass = (buttonClassNames: string[]) => {
   });
 };
 
+// function to check if screen is null
+const isScreenNull = (element: HTMLElement | null) => {
+  if (typeof element === null) {
+    throw new Error("Problem with screen element");
+  }
+  return false;
+};
+
+// function to check if button is null
+const isButtonNull = (eventTarget: HTMLButtonElement) => {
+  if (typeof eventTarget === null) {
+    throw new Error("Problem with button element");
+  }
+  return eventTarget;
+};
+
+// function to display a default value when load page
+const displayValue = (value: string) => {
+  if (isScreenNull(screen)) {
+    return;
+  } else {
+    screen!.innerText = value;
+    return `test`;
+  }
+};
+
+//function to update the display
+
+const updateDisplay = (buttonClicked: HTMLButtonElement) => {
+  const value = buttonClicked.textContent;
+  displayValue(value!);
+};
+
+// function to handle button click
+const handleClick = (event: Event) => {
+  // storing clicked button and casting to HTMLButtonELement from EventTarget
+  const button = event.currentTarget as HTMLButtonElement;
+  const validButton = isButtonNull(button);
+  updateDisplay(validButton);
+};
+
+// function to get button elements and store in TS array
+
+const getButtons = () => {
+  const buttonElements = document.querySelectorAll("button");
+  buttonElements.forEach((element: HTMLButtonElement) => {
+    buttons.push(element);
+  });
+};
+
+// operation functions
+const add = (numberArray: number[]) => {
+  let result: number = 0;
+  numberArray.forEach((number: number) => {
+    result += number;
+  });
+  return result;
+};
+
+const subtract = (numberArray: number[]) => {
+  let result: number = numberArray[0] - numberArray[1];
+  return result;
+};
+
+// function calls
+/* _______________________________________________________________________*/
 giveButtonClass(buttonClassNames);
+
+getButtons();
+
+buttons.forEach((button) => {
+  button.addEventListener("click", handleClick);
+});
