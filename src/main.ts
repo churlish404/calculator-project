f278b3264b381e016a0e24f4fd4b4c90809273bf
lucaGilardenghi <luca.gilardenghi@yahoo.co.uk>
2023-07-31 20:39:17 +0100

    Added handleClick()

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import "./style.scss";
 
 // strings array to store text on buttons and assign grid area
-
 const buttonClassNames: string[] = [
-  "cancel",
+  "clear-all",
   "sign",
   "percentage",
   "divide",
@@ -24,6 +23,15 @@ const buttonClassNames: string[] = [
   "decimal",
 ];
 
+// get all HTML button elements store in an array
+const buttons = document.querySelectorAll("button");
+
+// checking in case querySelectorAll() has returned empty NodeList
+if (buttons.length === 0) {
+  throw new Error("problem retrieving HTML buttons");
+}
+
+// function declarations
 const giveButtonClass = (buttonClassNames: string[]) => {
   const buttons = document.querySelectorAll("button");
   let counter = 0;
@@ -33,4 +41,25 @@ const giveButtonClass = (buttonClassNames: string[]) => {
   });
 };
 
+// register button click store value of button
+const handleButtonClick = (event: Event) => {
+  const button = event.currentTarget as HTMLButtonElement;
+
+  // if is clear button call clear() function
+  if (button.classList.contains("clear-all")) {
+    // call clear function here
+    console.log("clear");
+  } else {
+    const buttonValue: string = button.textContent!;
+    return buttonValue;
+  }
+};
+
+// function calls
+
 giveButtonClass(buttonClassNames);
+
+//add event listener to all buttons
+buttons.forEach((button) => {
+  button.addEventListener("click", handleButtonClick);
+});
